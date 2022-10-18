0198b5afda73cecfae1de5fdf12e77cdb2c1da2b
SirNemezis <490vbt@gmail.com>
2022-10-18 13:59:54 +0300

    Украл дизайн

--- anime_merch_website/static/js/cart/cart.js
+++ anime_merch_website/static/js/cart/cart.js
@@ -2,6 +2,7 @@ function change_price(a, b){
     var p = a.parentElement;
     var i = p.children[1];
     i.value = Number(i.value) + b;
+    if (Number(i.value) === 0){delete_row(p)}
     calculate_total_price(a)
 }
 
@@ -9,6 +10,8 @@ function calculate_total_price(a){
     var q = a.parentElement.children[1].value;
     var tp = a.parentElement.parentElement.parentElement.children[4];
     var cp = a.parentElement.parentElement.parentElement.children[2].children[0].innerHTML;
-    console.log(cp[0])
     tp.children[0].innerHTML = cp[0] + String(q * Number(cp.substring(1)))
+}
+function delete_row(e){
+    e.parentElement.parentElement.remove()
 }
